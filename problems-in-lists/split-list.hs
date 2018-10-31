7be7a893bd90cf74312a6790fcde8dd0378e237c
nmjmdr <narasimha.gm@gmail.com>
2018-10-31 16:39:46 +1100

    split at

--- problems-in-lists/split-list.hs
+++ problems-in-lists/split-list.hs
@@ -9,3 +9,11 @@ splitAtIndex n index part (x:xs) = if n == index
 splitat :: Int -> [a] -> ([a],[a])
 splitat _ [] = ([],[])
 splitat n xs = splitAtIndex n 0 [] xs
+
+splitat' :: Int -> [a] -> ([a],[a])
+splitat' _ [] = ([],[])
+splitat' n (x:xs) = if n > 0 
+                     then let (ys,zs) = splitat' (n-1) xs in (x:ys,zs)
+                     else ([],(x:xs))
+
+
